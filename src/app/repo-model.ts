958141b4ea7d404ffa0b26f955377ca7195d8b1c
thuojose <joseph.thuo@student.moringaschool.com>
2021-08-29 21:14:00 +0300

    added repo-model.ts

---
+++ src/app/repo-model.ts
@@ -0,0 +1,11 @@
+export class RepoModel {
+
+    description: any;
+    name: any;
+    owner: any;
+    watchers: any;
+    forks: any;
+    default_branch: any;
+    language: any;
+    searchTerms:any;
+}
