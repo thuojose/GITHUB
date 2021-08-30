abb1f8ff9443040ad54f8f0e476005e373984d8a
thuojose <joseph.thuo@student.moringaschool.com>
2021-08-30 12:34:12 +0300

    added pipe content

--- src/app/data-count.pipe.ts
+++ src/app/data-count.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DataCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  transform(value: any): number {
+    let today:Date =new Date();//get current date ad time
+    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate(),today.getTime())
+    var dateDifference = Math.abs(todayWithNoTime-value)//return value in milliseconds
+    const secondsInDay =86400; //seconds * 60 miutes in an hour * 24 hours in a day
+    var dateDifferenceSeconds = dateDifference*0.001; //converts milliseconds to seconds
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
 
+    if (dateCounter >= 1 && value >todayWithNoTime){
+    return dateCounter;
+  }
+  else{
+    return 0;
+  }
+  }
 }
